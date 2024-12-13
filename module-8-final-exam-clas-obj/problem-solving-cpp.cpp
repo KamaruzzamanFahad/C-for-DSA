dd2c0a5f778ca01e440738e7450ab221ace1f65d
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2024-12-13 09:01:17 +0600

    update

---
+++ module-8-final-exam-clas-obj/problem-solving-cpp.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string s;
+    cin >> s;
+
+    stringstream ss (s);
+    string word;
+
+    while(ss >> word){
+        cout << word << endl;
+    }
+
+
+}
